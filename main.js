833f930ef23b7ffca5740e3aa72571c6898c0c57
Yichao <403624799@qq.com>
2020-09-26 21:33:50 +0800

    用range去更新元素，实现setState

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 import {createElement, render, Component} from './toy-react'
 
 class MyComponent extends Component{
+  constructor(){
+    super();
+    this.state = {
+      a: 1,
+      b: 2
+    }
+  }
   render(){
     return <div>
       <h1>MyComponent</h1>
-      {this.children}
+      <button onclick = {()=>{ this.setState({a:this.state.a + 1}); }}>add</button>
+      <span>{this.state.a.toString()}</span>
+      <br></br>
+      <span>{this.state.b.toString()}</span>
     </div>
   }
 }
--- toy-react.js
+++ toy-react.js
@@ -1,14 +1,42 @@
 
+// symbol不需要new，没有private field之前最好的方案是用symbol
+const RENDER_TO_DOM = Symbol("render to dom");
 // 
  class ElementWrapper {
   constructor(type){
     this.root = document.createElement(type);
   }
   setAttribute(name,value){
-    this.root.setAttribute(name, value);
+    // 过滤以on开头的属性
+    // 用了正则表达式， \s和\S一个所有的空白，一个是所有的非空白
+    // [\s\S]这是正则里面用来表示所有字符的一个比较稳妥的方式
+    // 
+    if(name.match(/^on([\s\S]+)/)){
+      // 上面的正则加了括号，下面的RegExp.$1就可以提取到对应的值
+      this.root.addEventListener(RegExp.$1.replace(/^[\s\S]/, c => c.toLowerCase()), value);
+    }else{
+      this.root.setAttribute(name, value);
+    }
   }
   appendChild(component){
-    this.root.appendChild(component.root);
+    let range = document.createRange();
+    range.setStart(this.root, this.root.childNodes.length);
+    range.setEnd(this.root, this.root.childNodes.length);
+    component[RENDER_TO_DOM](range);
+  }
+  [RENDER_TO_DOM](range){
+    range.deleteContents();
+    range.insertNode(this.root);
+  }
+}
+
+class TextWrapper {
+  constructor(content){
+    this.root = document.createTextNode(content);
+  }
+  [RENDER_TO_DOM](range){
+    range.deleteContents();
+    range.insertNode(this.root);
   }
 }
 
@@ -17,6 +45,7 @@ export class Component {
     this.props = Object.create(null);
     this.children = [];
     this._root = null;
+    this._range = null;
   }
   setAttribute(name,value){
     this.props[name] = value;
@@ -24,28 +53,52 @@ export class Component {
   appendChild(component){
     this.children.push(component);
   }
-  get root(){
-    if(!this._root){
-      this._root = this.render().root;
+  // 私有方法
+  // rangeAPI 和 位置相关
+  // domAPI里面和位置相关的是range
+  [RENDER_TO_DOM](range){
+    this._range = range;
+    this.render()[RENDER_TO_DOM](range);
+  }
+
+  // 写一个重新绘制的方法
+  rerender(){
+    // console.log("rerender--执行",this._range);
+    this._range.deleteContents();
+    this[RENDER_TO_DOM](this._range);
+  }
+  // 实现setState的方法
+  setState(newState){
+    // 加入state不存在，直接赋值，这是一个短路逻辑
+    if(this.state === null || typeof this.state !== 'object'){
+      this.state = newState;
+      this.rerender();
+      return;
     }
-    return this._root;
+    let merge = (oldState, newState) => {
+      for(let p in newState){
+        if(oldState[p] === null || typeof newState[p] !== 'object' ){
+          oldState[p] = newState[p];
+        }else{
+          // 剩下这种情况，说明newState[p]是一个object
+          // 所以要执行深拷贝
+          merge(oldState[p], newState[p]);
+        }
+      }
+    }
+    merge(this.state, newState);
+    this.rerender();
   }
 }
 
-class TextWrapper {
-  constructor(content){
-    this.root = document.createTextNode(content);
-  }
-  setAttribute(name,value){
 
-  }
-  appendChild(){
-    
-  }
-}
 
 export function render(component, parentElement){
-  parentElement.appendChild(component.root);
+  let range = document.createRange();
+  range.setStart(parentElement, 0);
+  range.setEnd(parentElement, parentElement.childNodes.length);
+  range.deleteContents();
+  component[RENDER_TO_DOM](range);
 }
 
 export function createElement(type, attributes, ...children){
